ed61e3a7d44ffaa04377a44737007745642d5fd7
Fabrice Magnan <magnan.fabrice@gmail.com>
2023-12-15 17:24:22 +0100

    correction scss

--- src/pages/entreprise/ProjetEntreprise.jsx
+++ src/pages/entreprise/ProjetEntreprise.jsx
@@ -23,7 +23,7 @@ const ProjetEntreprise = () => {
       theme: "Création d'un outil en VBA Excel pour automatiser des tâches de contrôles comptables",
       description:
         "Ma mission a consisté à créer une interface web utilisateur pour remplir un questionnaire métier et ainsi permette l'intégration en automatique dans une application interne utilisant la technologie vb.net",
-      languages: [{ name: "VBA", logo: LogoVba }],
+      languages: [{ name: "VBA", logo: LogoVba, isLarge: true }],
       url: "https://fabwebprojects.fr/Riding_Cities/index.html",
     },
   ];
@@ -38,9 +38,10 @@ const ProjetEntreprise = () => {
             </h2>
             <p className="project-theme">{project.theme}</p>
             <p className="project-description">{project.description}</p>
+            <h3 className="technologies-used">Technologie(s) utilisée(s) :</h3>
             <div className="project-technologies">
               {project.languages.map((lang, langIndex) => (
-                <img key={langIndex} src={lang.logo} alt={`${lang.name} Logo`} className="technology-logo" />
+                <img key={langIndex} src={lang.logo} alt={`${lang.name} Logo`} className={`technology-logo ${lang.isLarge ? "large-technology-logo" : ""}`} />
               ))}
             </div>
             <a href={project.url} target="_blank" rel="noopener noreferrer" className="project-view-button">
