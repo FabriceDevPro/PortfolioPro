4b53a096d2fbd766a34f6dd633da352a34f32d6c
Fabrice-Perso <magnan.fabrice@gmail.com>
2024-03-03 15:42:41 +0100

    correction scss responsive

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ function App() {
           </Routes>
         </main>
         <AppWrapper />
-        <ScrollToTop />
+        <ScrollToTop/>
       </Router>
     </ContactModalProvider>
     </SkillsProvider>
