faf49e3050155b1ded115388096a3038f7a3d0ed
Fabrice-Perso <magnan.fabrice@gmail.com>
2024-03-29 17:46:05 +0100

    Version 2.0.3 Portfolio - Mise en place Traduction Anglais-Francais + amélioration style - correction de texte

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/portfoliotest" : "/",
+  base: process.env.NODE_ENV === "production" ? "/" : "/",
   server: {
     historyApiFallback: true,
   },
